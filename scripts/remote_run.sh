3f1b537df398196e77ca2d342e985a8688cf33b5
dmitrenkoandrey <werdnx@gmail.com>
2020-07-07 23:04:16 +0300

    CPU test

--- scripts/remote_run.sh
+++ scripts/remote_run.sh
@@ -3,4 +3,4 @@
 R_HOST=192.168.1.78
 sshpass -p "werdn" ssh werdn@$R_HOST 'DEST=/media/3tstor/ml/IdeaProjects/ml_kaggle_competition1; cd $DEST/docker; docker build --tag ml_kaggle1:1.0 .'
 sshpass -p "werdn" ssh werdn@$R_HOST 'DEST=/media/3tstor/ml/IdeaProjects/ml_kaggle_competition1; cd $DEST/docker; docker rm kaggle_container'
-sshpass -p "werdn" ssh werdn@$R_HOST 'DEST=/media/3tstor/ml/IdeaProjects/ml_kaggle_competition1; cd $DEST/docker; docker run --gpus all --name kaggle_container -v $DEST/input:/input -v $DEST/output:/output -v $DEST/python:/python ml_kaggle1:1.0'
+sshpass -p "werdn" ssh werdn@$R_HOST 'DEST=/media/3tstor/ml/IdeaProjects/ml_kaggle_competition1; cd $DEST/docker; docker run --name kaggle_container -v $DEST/input:/input -v $DEST/output:/output -v $DEST/python:/python ml_kaggle1:1.0'

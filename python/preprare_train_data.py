7c6162a639e76cb2ed957c6fc74e20c9f2936708
dmitrenkoandrey <werdnx@gmail.com>
2020-07-10 19:22:00 +0300

    add chunks for learning and script for rotating of positive cases

--- python/preprare_train_data.py
+++ python/preprare_train_data.py
@@ -15,7 +15,7 @@ def read_csv(path):
                 print('Column names are ', ", ".join(row))
                 line_count += 1
             else:
-                if row[7] == 0:
+                if row[7] == "0":
                     negative.append((row[0], row[7]))
                 else:
                     positive.append((row[0], row[7]))
@@ -26,10 +26,12 @@ def read_csv(path):
 
 def split_data():
     pos, neg = read_csv("/input/train.csv")
+    print("positive cases " + len(pos))
+    print("negative cases " + len(neg))
     train_pos, test_pos = train_test_split(pos, test_size=0.33, random_state=42)
     train_neg, test_neg = train_test_split(neg, test_size=0.33, random_state=42)
     train = train_neg + train_pos
     shuffle(train)
     test = test_neg + test_pos
     shuffle(test)
-    return train, test
+    return train[:100], test[:100]
--- python/resnet_cut_image.py
+++ python/resnet_cut_image.py
@@ -53,10 +53,6 @@ def prepare_data(img_labels):
     count = 0
     for i, item in enumerate(img_labels):
         X[i, :] = read_image(DIR + item[0] + '.jpg')
-        if item[1] == 1:
-            y[0, i] = 1
-        else:
-            y[0, i] = 0
         count = count + 1
         if count % 100 == 0:
             logging.info("process load data, iter = %s", count)
@@ -68,27 +64,19 @@ def convert_to_one_hot(Y, C):
     return Y
 
 
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
 def run():
     logging.info("start split_data")
     train, test = split_data()
     logging.info("train count = %s", len(train))
     logging.info("test count = %s", len(test))
     logging.info("start prepare train")
-    train_set_x, train_set_y = prepare_data(train)
-    logging.info("start prepare test")
-    test_set_x, test_set_y = prepare_data(test)
-    X_train = train_set_x / 255
-    X_test = test_set_x / 255
-    logging.info("start reshape y train")
-    Y_train = convert_to_one_hot(train_set_y, CLASSES).T
-    logging.info("start reshape y test")
-    Y_test = convert_to_one_hot(test_set_y, CLASSES).T
-    logging.info("number of training examples = %s", X_train.shape[0])
-    logging.info("number of test examples = %s", X_test.shape[0])
-    logging.info("X_train shape: %s", X_train.shape)
-    logging.info("Y_train shape: %s", Y_train.shape)
-    logging.info("X_test shape: %s", X_test.shape)
-    logging.info("Y_test shape: %s", Y_test.shape)
+
     base_model = applications.resnet50.ResNet50(weights=None, include_top=False, input_shape=(ROWS, COLS, 3))
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
@@ -98,12 +86,32 @@ def run():
     # sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
     adam = Adam(lr=0.0001)
     model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(X_train, Y_train, epochs=5, batch_size=16)
+
+    train_chunks = chunks(train, 10)
+    for train_chunk in train_chunks:
+        train_set_x, train_set_y = prepare_data(train_chunk)
+        X_train = train_set_x / 255
+        logging.info("start reshape y train")
+        Y_train = convert_to_one_hot(train_set_y, CLASSES).T
+        logging.info("number of training examples = %s", X_train.shape[0])
+        logging.info("X_train shape: %s", X_train.shape)
+        logging.info("Y_train shape: %s", Y_train.shape)
+        model.fit(X_train, Y_train, epochs=3, batch_size=8)
+
+    logging.info("start prepare test")
+    test_set_x, test_set_y = prepare_data(test)
+    X_test = test_set_x / 255
+    logging.info("start reshape y test")
+    Y_test = convert_to_one_hot(test_set_y, CLASSES).T
+    logging.info("number of test examples = %s", X_test.shape[0])
+    logging.info("X_test shape: %s", X_test.shape)
+    logging.info("Y_test shape: %s", Y_test.shape)
+
     preds = model.evaluate(X_test, Y_test)
     logging.info("Loss = %s" + str(preds[0]))
     logging.info("Test Accuracy = %s" + str(preds[1]))
     logging.info("model saved")
-    model.save('/input/model1_50rsnet_256_256_10_epoch')
+    model.save('/input/model1_50rsnet_256_256_10_epoch2')
     logging.info("preds ")
     for x in range(len(preds)):
         print (preds[x])
